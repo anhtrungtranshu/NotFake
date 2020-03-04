97ef445cba8888832eb6c5679a459ee853b9b157
Tran <b9035740@hallam.shu.ac.uk>
2020-03-04 11:25:17 +0000

    second commit

---
+++ Service/IRepository/IRepositoryBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Service.IRepository
+{
+    public interface IRepositoryBase<T>
+    {
+        T Get(int id);
+        IList<T> ListAll();
+        int Update(T entity);
+        int Delete(T entity);
+    }
+}
