e48d02408d361dd3c9d31191148b7843d1a3851e
Tran <b9035740@hallam.shu.ac.uk>
2020-03-04 13:52:43 +0000

    add gitignore

---
+++ DAO/Models/Film.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace DAO.Models
+{
+    class Film
+    {
+        private string FilmName;
+
+        [Key]
+        int Id { get; set; }
+        
+        public string Name {get; set;}
+
+
+
+    }
+}
---
+++ Service/NotFakeService.cs
@@ -0,0 +1,20 @@
+﻿using DAO;
+using Service.IRepository;
+using Service.Repository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Service
+{
+    public class NotFakeService : INotFakeService
+    {
+        public NotFakeService()
+        {
+            NotFakeContext context = new NotFakeContext();
+            User = new UserRepository(context);
+        }
+        IUserRepository User { get; set; }
+
+    }
+}
