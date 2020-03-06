33c00f248a507ee25a7019766390ec3185152f17
Newcombe <b9034710@hallam.shu.ac.uk>
2020-03-06 12:53:20 +0000

    ClaudeModelsCompleteReload

---
+++ DAO/Models/Friendship.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace DAO.Models
+{
+    public class Friendship
+    {
+        [Key]
+        [ForeignKey("User")]
+        int User1ID { get; set; }
+        [Key]
+        [ForeignKey("User")]
+        int User2ID { get; set; }
+        [Required]
+        string status { get; set; }
+    }
+}
---
+++ DAO/Models/Post.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace DAO.Models
+{
+    public class Post
+    {
+        [Key]
+        [ForeignKey("User")]
+        int User1ID { get; set; }
+        [Key]
+        [ForeignKey("User")]
+        int User2ID { get; set; }
+        [Required]
+        string Content { get; set; }
+        [Required]
+        DateTime Created { get; set; }
+    }
+}
