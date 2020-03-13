9c277b307b69da5b7ad06241104cec7c3016dc19
claude-newcombe <claudenewcombe94@gmail.com>
2020-03-13 12:12:51 +0000

    finalAuth

--- NotFake/Controllers/AuthController.cs
+++ NotFake/Controllers/AuthController.cs
@@ -32,7 +32,7 @@ namespace NotFake.Controllers
                 //AllowRefresh = <bool>,
                 // Refreshing the authentication session should be allowed.
 
-                //ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(10),
+                ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(1440)
                 // The time at which the authentication ticket expires. A 
                 // value set here overrides the ExpireTimeSpan option of 
                 // CookieAuthenticationOptions set with AddCookie.
