9ac87b33a47b3d5139dcfa0059cda4af2186676c
RuchiChopra43 <56389557+RuchiChopra43@users.noreply.github.com>
2020-03-16 09:44:55 +0000

    Register

---
+++ NotFake/Models/Utility.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace NotFake.Models
+{
+    public class Utility
+    {
+        public static bool IsEmailValid(string Email)
+        {
+            try
+            {
+                MailAddress m = new MailAddress(Email);
+
+                return true;
+            }
+            catch (FormatException)
+            {
+                return false;
+            }
+        }
+
+
+        //private bool IsValidUser(string userName, string passWord)
+
+        //{
+
+
+        //}
+
+
+    }
+}
