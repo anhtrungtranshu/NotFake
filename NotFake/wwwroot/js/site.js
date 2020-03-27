6c346731a2cd79ec06ef57156629db1fe9ca7c15
anhtrungtranshu <b9035740@my.shu.ac.uk>
2020-03-27 00:05:59 +0000

    slick carousel instead of Bootstrap carousel

--- NotFake/wwwroot/js/site.js
+++ NotFake/wwwroot/js/site.js
@@ -6,4 +6,14 @@ $(document).ready(function () {
     $(function () {
         $('[data-toggle="tooltip"]').tooltip()
     });
+
+    // slick
+    $(".slick-carousel").slick({
+        lazyLoad: 'ondemand',
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        variableWidth: true,
+        prevArrow: "<button class='slick-prev slick-arrow'><i class='fas fa-chevron-left'></i></button>",
+        nextArrow: "<button class='slick-next slick-arrow'><i class='fas fa-chevron-right'></i></button>",
+    })
 })
