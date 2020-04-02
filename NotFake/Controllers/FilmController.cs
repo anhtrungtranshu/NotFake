2b99ebf3eeebb6c89546d74b2c04a0893917213d
claude-newcombe <claudenewcombe94@gmail.com>
2020-04-02 16:09:50 +0100

    chatUserFromViewBag

--- NotFake/Controllers/FilmController.cs
+++ NotFake/Controllers/FilmController.cs
@@ -3,20 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 
 namespace NotFake.Controllers
 {
     public class FilmController : Controller
     {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        public string userName { get; set; }
+        public FilmController(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+            if (_httpContextAccessor.HttpContext.User.Identity.IsAuthenticated)
+            {
+                userName = _httpContextAccessor.HttpContext.User.Identity.Name;
+            }
+        }
         public IActionResult Index(int id)
         {
             ViewBag.FilmId = id;
+            ViewBag.UserId = userName;
             return View();
         }
 
         public IActionResult Chat()
         {
-            ViewBag.UserId = HttpContext.User.Identity;
+            
             return View("Chat");
         }
     }
